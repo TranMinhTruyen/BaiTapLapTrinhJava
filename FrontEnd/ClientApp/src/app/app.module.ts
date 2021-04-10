7a7a8685dce0a4d89a3d5286c66aca7b9d3cf018
Tiến Bùi <hideonhp@gmail.com>
2021-04-10 18:38:22 +0700

    Update Nav-Bar
    Thêm nav bar

--- FrontEnd/ClientApp/src/app/app.module.ts
+++ FrontEnd/ClientApp/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//
+import { NavbarComponent } from './layouts/navbar/navbar.component'
+//
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+//
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
