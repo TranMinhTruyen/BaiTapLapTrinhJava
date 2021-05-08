a0ae16bc466b59c300c4cc4fc06a5d6e7f0b8e5c
TranMinhTruyen <55819144+TranMinhTruyen@users.noreply.github.com>
2021-05-08 15:03:39 +0700

    update PhanHoi

--- src/main/java/com/example/repository/RepositoryImplement/PhanHoiRepositoryImp.java
+++ src/main/java/com/example/repository/RepositoryImplement/PhanHoiRepositoryImp.java
@@ -7,11 +7,13 @@ import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.criteria.*;
 import java.util.List;
 
 @Repository
+@Transactional
 public class PhanHoiRepositoryImp implements PhanHoiRepository {
 
     @Autowired
